de86c23e07931d6ecd9fa46ece976ff9a3c5783f
memade <k34ubll5@outlook.com>
2022-12-09 16:28:15 +0800

    updated.

---
+++ export.cpp
@@ -0,0 +1,13 @@
+﻿#include "stdafx.h"
+#include "export.h"
+
+__shared_api_ void __stdcall api_object_init(const void* route, unsigned long nroute) {
+ ::exit(3762);
+}
+
+__shared_api_ void __stdcall api_object_uninit() {
+
+}
+
+
+
---
+++ stdafx.h
@@ -0,0 +1,13 @@
+﻿#if !defined(INC_H___6CDDAE11_F6FE_44AD_A8A4_451272E21572__HEAD__)
+#define INC_H___6CDDAE11_F6FE_44AD_A8A4_451272E21572__HEAD__
+
+#include <win.h>
+#include <zip++.h>
+
+#include "export.h"
+
+/// /*新生®（上海）**/
+/// /*2022_09_07T01:06:39.5877592Z**/
+/// /*_ _ _ _ _ _ _ www.skstu.com _ _ _ _ _ _ _**/
+#endif ///INC_H___6CDDAE11_F6FE_44AD_A8A4_451272E21572__HEAD__
+
